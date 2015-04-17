cbb4ab560b77aad7a8b63bba45a1c2674a05d357
Victor Englund <victor.englund@gmail.com>
2015-04-17 11:46:37 +0200

    Added functionality write to file

--- src/DownloadThread.java
+++ src/DownloadThread.java
@@ -1,4 +1,5 @@
 import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.io.File;
@@ -15,10 +16,20 @@ public class DownloadThread extends Thread {
 		try {
 			BufferedInputStream in = new BufferedInputStream(this.socket.getInputStream());
 			File file = new File("test.txt");
+			FileOutputStream out = new FileOutputStream(file);
+			byte[] buffer = new byte[8192];
+			int dataLength = 0;
+			while((dataLength = in.read(buffer, 0, buffer.length)) > 0) {
+				out.write(buffer, 0, dataLength);
+			}
+			out.flush();
+			out.close();
+			in.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	
 	
 }
