2903a5d9f553e86e3f3414d437cebacfde98c59d
Victor Englund <victor.englund@gmail.com>
2015-05-19 12:57:20 +0200

    Check correct instance of the object of the transfer

--- src/TransferJTable.java
+++ src/TransferJTable.java
@@ -2,7 +2,6 @@ import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 
 public class TransferJTable extends JTable implements Observer {
@@ -13,11 +12,12 @@ public class TransferJTable extends JTable implements Observer {
 
 	@Override
 	public void update(Observable observable, Object object) {
-		// TODO: check correct class types before type casting!
 		TransferMonitor transferMonitor = (TransferMonitor) observable;
-		ClientThread client = (ClientThread) object;
-		TransferTableModel model = (TransferTableModel) getModel();
-		client.addObserver(model);		
-		model.addTransfer(client);
+		if (object instanceof ClientThread) {
+			ClientThread client = (ClientThread) object;
+			TransferTableModel model = (TransferTableModel) getModel();
+			client.addObserver(model);
+			model.addTransfer(client);
+		}
 	}
 }
